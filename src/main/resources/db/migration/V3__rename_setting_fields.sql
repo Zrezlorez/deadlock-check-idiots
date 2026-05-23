9b46f158e74377c0c76a1749d6e5b7a604fa93a7
Тимофей Чудаков <tvchudakov@spasibosb.ru>
2026-05-23 18:42:23 +0300

    hotfix migration

--- src/main/resources/db/migration/V3__rename_setting_fields.sql
+++ src/main/resources/db/migration/V3__rename_setting_fields.sql
@@ -1,11 +1,11 @@
-update app_settings set setting_value = 'fuck_cost_percent' where setting_value = 'enemy_fail_cost_percent';
-update app_settings set setting_value = 'enemy_fail_chance_penalty' where setting_value = 'fuck_fail_chance_penalty';
-update app_settings set setting_value = 'enemy_growth_penalty' where setting_value = 'slow_growth_penalty';
-update app_settings set setting_value = 'self_fail_chance_penalty' where setting_value = 'jackpot_fail_chance';
-update app_settings set setting_value = 'self_crit_chance_bonus' where setting_value = 'jackpot_crit_chance';
-update app_settings set setting_value = 'self_fail_bonus' where setting_value = 'pray_fail_bonus';
-update app_settings set setting_value = 'self_growth_bonus' where setting_value = 'turtle_growth_bonus';
-update app_settings set setting_value = 'max_pending_fail_chance_penalty' where setting_value = 'max_pending_fail_chance';
-update app_settings set setting_value = 'max_pending_crit_chance_bonus' where setting_value = 'max_pending_crit_chance';
-update app_settings set setting_value = 'max_pending_growth_bonus' where setting_value = 'max_pending_growth';
-delete from app_settings where setting_value = 'max_pending_growth_penalty'
+update app_settings set setting_key = 'fuck_cost_percent' where setting_key = 'enemy_fail_cost_percent';
+update app_settings set setting_key = 'enemy_fail_chance_penalty' where setting_key = 'fuck_fail_chance_penalty';
+update app_settings set setting_key = 'enemy_growth_penalty' where setting_key = 'slow_growth_penalty';
+update app_settings set setting_key = 'self_fail_chance_penalty' where setting_key = 'jackpot_fail_chance';
+update app_settings set setting_key = 'self_crit_chance_bonus' where setting_key = 'jackpot_crit_chance';
+update app_settings set setting_key = 'self_fail_bonus' where setting_key = 'pray_fail_bonus';
+update app_settings set setting_key = 'self_growth_bonus' where setting_key = 'turtle_growth_bonus';
+update app_settings set setting_key = 'max_pending_fail_chance_penalty' where setting_key = 'max_pending_fail_chance';
+update app_settings set setting_key = 'max_pending_crit_chance_bonus' where setting_key = 'max_pending_crit_chance';
+update app_settings set setting_key = 'max_pending_growth_bonus' where setting_key = 'max_pending_growth';
+delete from app_settings where setting_key = 'max_pending_growth_penalty'
