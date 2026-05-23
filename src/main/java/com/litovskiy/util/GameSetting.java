a33650c15f0324e2e758a6c3b15cd703212fe99a
Тимофей Чудаков <tvchudakov@spasibosb.ru>
2026-05-23 16:35:49 +0300

    migration game setting

--- src/main/java/com/litovskiy/util/GameSetting.java
+++ src/main/java/com/litovskiy/util/GameSetting.java
@@ -4,10 +4,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Locale;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Getter
