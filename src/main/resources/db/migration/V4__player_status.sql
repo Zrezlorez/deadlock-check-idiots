91c7bf661dde54d00e6d6a509622aea50bd3a1a2
Тимофей Чудаков <tvchudakov@spasibosb.ru>
2026-05-25 21:28:03 +0300

    fixes

--- src/main/resources/db/migration/V4__player_status.sql
+++ src/main/resources/db/migration/V4__player_status.sql
@@ -1,7 +1,7 @@
 alter table players add column if not exists status varchar(64);
 alter table players add column if not exists status_until timestamptz;
 
-alter table player_growth_stats add column if not exists average_growth double precision;
+alter table player_growth_stats add column if not exists average_growth double precision default 0 not null;
 
 alter table player_behavior_stats add column if not exists last_ability_action varchar(64);
 alter table player_behavior_stats add column if not exists same_ability_streak integer;
